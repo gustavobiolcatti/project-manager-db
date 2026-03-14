05c6862bb9d0cac1c5f51d1be63917d69cdbd150
gustavobiolcatti <gustavobiolcatti@hotmail.com>
2026-03-14 12:32:45 -0300

    feat: rename package to `project-manager-db`, add `main`, `types`, `files`, and `prepare` script, and export enums and entities directly from `bootstrap.ts`.

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,3 +1,7 @@
 import * as ProjectManagerDb from './project-manager-db/entity';
+import * as ProjectManagerDbEnums from './project-manager-db/enums';
 
-export { ProjectManagerDb };
+export { ProjectManagerDb, ProjectManagerDbEnums };
+
+export * from './project-manager-db/entity';
+export * from './project-manager-db/enums';
